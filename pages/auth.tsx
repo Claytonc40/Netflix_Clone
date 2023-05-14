5e331f766d580319d9dce4afb7219600cf62adb5
Claytonc40 <clayton_c40@hotmail.com>
2023-05-14 14:27:26 -0300

    Interface de Login Pronta

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import Input from "./components/input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 export default function Auth() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -13,14 +22,18 @@ export default function Auth() {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Entrar</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Entrar" : "Cadastrar"}
+            </h2>
             <div className="flex flex-col gap-4">
-              <Input
-                id="name"
-                label="Nome de Usuário"
-                onChange={(ev: any) => setName(ev.target.value)}
-                value={name}
-              />
+              {variant === "register" && (
+                <Input
+                  id="name"
+                  label="Nome de Usuário"
+                  onChange={(ev: any) => setName(ev.target.value)}
+                  value={name}
+                />
+              )}
               <Input
                 id="email"
                 label="E-mail"
@@ -39,12 +52,27 @@ export default function Auth() {
             <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               Entrar
             </button>
-            <p className="text-neutral-500 mt-12">
-              Novo por aqui?
-              <span className="text-white ml-1 hover:underline cursor-pointer">
-                Criar Conta
-              </span>
-            </p>
+            {variant === "login" ? (
+              <p className="text-neutral-500 mt-12">
+                Novo por aqui?
+                <span
+                  onClick={toggleVariant}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                  Criar Conta
+                </span>
+              </p>
+            ) : (
+              <p className="text-neutral-500 mt-12">
+                Já tem uma Conta?
+                <span
+                  onClick={toggleVariant}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                  Entar
+                </span>
+              </p>
+            )}
           </div>
         </div>
       </div>
